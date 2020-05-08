a02be63e8c23407ac3a37b8f26443270bddec8b1
Julian Mueller <julian.mueller@immatics.com>
2020-05-08 14:30:13 +0200

    Small Bugfix

--- prosit/train_prosit_RT.py
+++ prosit/train_prosit_RT.py
@@ -36,15 +36,14 @@ for is_trained in [True, False]:
 	training_tensorized = tensorize.csv(training_df[['modified_sequence', 'collision_energy', 'precursor_charge']], nlosses=3)
 	print("CSV Tensorized.")
 	training_tensorized['prediction'] = np.reshape(
-		np.asarray((training_df.uRT - iRT_raw_mean) / np.sqrt(iRT_raw_var)),
-		(-1,1))
+		np.asarray((training_df.uRT - iRT_raw_mean) / np.sqrt(iRT_raw_var)),(-1,1))
 
 	#Write mean and var of iRTs to config file, they will be used at prediction time for scaling the results properly
 	iRT_config['iRT_rescaling_mean'] = float(iRT_raw_mean)
-		iRT_config['iRT_rescaling_var'] = float(iRT_raw_var)
-		with open(iRT_model_dir + "config_new.yml", "w") as config_outfile:
-		    yaml.dump(iRT_config, config_outfile)
-		training.IMA_compile_model(iRT_model, iRT_config)
+	iRT_config['iRT_rescaling_var'] = float(iRT_raw_var)
+	with open(iRT_model_dir + "config_new.yml", "w") as config_outfile:
+	    yaml.dump(iRT_config, config_outfile)
+	training.IMA_compile_model(iRT_model, iRT_config)
 
 	###Write and reload training data in hdf5 format
 	hdf5_path = "/root/training/training_data.hdf5"
@@ -58,7 +57,7 @@ for is_trained in [True, False]:
 	iRT_history = training.IMA_train(training_loaded, iRT_model, iRT_config, iRT_callbacks)
 	iRT_epochs = len(iRT_history.history['val_loss'])
 	iRT_val_loss = iRT_history.history['val_loss'][-1]
-	iRT_weights_filename = "{}/weight_{:02d}_{:.5f}.hdf5".format(iRT_model_dir, iRT_epochs, iRT_val_loss)
+	iRT_weights_filename = "{}/weight_{:02d}_{:.5f}{}.hdf5".format(iRT_model_dir, iRT_epochs, iRT_val_loss, "_transfer_trained" if is_trained else "")
 	iRT_model.save_weights(iRT_weights_filename)
 	print("Training of iRT Model Complete.")
 print("Done! You may now use these two models for your predictions.")
