8c2c32823ba221ec359cce1ddeef16a64237f3df
root <root@DETUE1LIN005.localdomain>
2020-07-17 15:34:09 +0200

    Minor Bugfixes

--- prosit/converters/rtlist.py
+++ prosit/converters/rtlist.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 
 class Converter:
-	def __init__(self, data, out_path):
-		self.data = data
-		self.out_path = out_path
+    def __init__(self, data, out_path):
+        self.data = data
+        self.out_path = out_path
 
-	def convert(self):
-		res = pd.DataFrame({
-			"modified_sequence" : [utils.get_sequence(seq_int) for seq_int in self.data["sequence_integer"]],
-			"iRT" : self.data["iRT"]
-			})
-		res[['modified_sequence', 'iRT']].to_csv(self.out_path, sep="\t", index=False)
+    def convert(self):
+        res = pd.DataFrame({
+            "modified_sequence" : [utils.get_sequence(seq_int) for seq_int in self.data["sequence_integer"]],
+            "iRT" : np.hstack(self.data["iRT"])
+            })
+        res[['modified_sequence', 'iRT']].to_csv(self.out_path, sep="\t", index=False)
--- prosit/train_prosit_RT.py
+++ prosit/train_prosit_RT.py
@@ -10,10 +10,13 @@ from . import training, tensorize, io_local, constants
 #Turn off warnings:
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-#Load both training files at once and concatenate (for iRT, the fragmentation method does not matter so we can use both)
-training_data_paths =  ["/root/training/IM_HCD_25_27_train_1_preprocessed.csv", "/root/training/IM_CID_35_train_1_preprocessed.csv"]
-training_dfs = [pd.read_csv(path) for path in training_data_paths]
-training_df = pd.concat(training_dfs)
+##Load both training files at once and concatenate (for iRT, the fragmentation method does not matter so we can use both)
+#training_data_paths =  ["/root/training/IM_HCD_25_27_train_1_preprocessed.csv", "/root/training/IM_CID_35_train_1_preprocessed.csv"]
+#training_dfs = [pd.read_csv(path) for path in training_data_paths]
+#training_df = pd.concat(training_dfs)
+#Load Training File
+training_df = pd.read_csv("/root/training/pepform_train.csv")
+
 
 ###Dump all Peptides containing selenocystein
 training_df = training_df.loc[~training_df.modified_sequence.str.contains("U")]
@@ -33,10 +36,10 @@ io_local.to_hdf5(training_tensorized,hdf5_path)
 print("Training Data Written to HDF5 File.")
 #Load the hdf5 again
 training_loaded = io_local.from_hdf5(hdf5_path)
-print("Training Data Reloaded from HDF5 File.\nCommencing Training of iRT Models (with and without pretrained weights.")
+print("Training Data Reloaded from HDF5 File.\nCommencing Training of iRT Model.")
 
 #The whole thing is run twice: Once with an untrained model and once with the pretrain model (to see if transfer learning is useful here)
-for is_trained in [True, False]:
+for is_trained in [True]: #, False]:
 	###Load  Retention Time Model and prepare its training data 
 	iRT_model_dir = "/root/training/IMA_uRT/"
 	iRT_model, iRT_config = model_lib.load(iRT_model_dir, trained=is_trained)
@@ -54,7 +57,7 @@ for is_trained in [True, False]:
 	iRT_history = training.IMA_train(training_loaded, iRT_model, iRT_config, iRT_callbacks)
 	iRT_epochs = len(iRT_history.history['val_loss'])
 	iRT_val_loss = iRT_history.history['val_loss'][-1]
-	iRT_weights_filename = "{}/{}weight_{:02d}_{:.5f}.hdf5".format(iRT_model_dir, "transfer_trained_" if is_trained else "", iRT_epochs, iRT_val_loss)
+	iRT_weights_filename = "{}/weight_{:02d}_{:.5f}.hdf5".format(iRT_model_dir,  iRT_epochs, iRT_val_loss)
 	iRT_model.save_weights(iRT_weights_filename)
 	print("Training of iRT Model Complete.")
-print("Done! You may now use these two models for your predictions.")
+print("Done! You may now use this model for your predictions.")
